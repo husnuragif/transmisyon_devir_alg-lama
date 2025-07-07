a30fbdf90c25d42cee005d8fc3be3c9d44fca1fa
Ali Gökalp Karakuş <rseyfeddinli6@gmail.com>
2025-07-08 01:25:41 +0300

    update

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function resetForm() {
   wheelDiameter.value = 0;
   kValue.value = 0;
   plateNumber.value = 0;
-  nTeker.value = 29.39;
+  nTeker.value = 0;
   seedSpacing.value = 0;
   gearRatioSelect.value = '';
   updateCalculations();
